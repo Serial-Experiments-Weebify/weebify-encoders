11fd0b823dc25b87e743a9663655377905368052
Matic Babnik <matic.babnik@outlook.com>
2022-04-11 08:24:33 +0200

    Shoutout to [Reaktor] for not setting stream language

--- syncoder/src/find-stream.js
+++ syncoder/src/find-stream.js
@@ -25,8 +25,13 @@ function findAudioStream(data, lang) {
 
     console.info(cyanBright(`[AUDIO] ${audioStreams.length} match language '${lang}'`));
     if (audioStreams.length == 0) {
-        console.warn(yellowBright('[AUDIO] No audio streams left!'));
-        return data.streams.filter(stream => stream.codec_type === 'audio')[0];
+        console.warn(yellowBright('[AUDIO] No audio streams left! Entering fallback mode.'));
+
+        let audioStreams = data.streams.filter(stream => stream.codec_type === 'audio');
+        let defaultAudio = audioStreams.find(stream => stream.disposition?.default > 0);
+
+        return defaultAudio ?? audioStreams[0]
+
     } else if (audioStreams.length == 1) {
         console.info(cyanBright('[AUDIO] Returning last remaining stream'));
         return audioStreams[0];
@@ -57,22 +62,33 @@ async function findSubStream(data, lang) {
         return null;
     }
 
-    subs = subs.filter(sub => sub.tags.language === lang);
+    let langSubs = subs.filter(sub => sub.tags.language === lang);
 
-    console.info(cyanBright(`[SUBS] ${subs.length} match language '${lang}'`));
-    if (subs.length == 0) {
-        console.warn(yellowBright('[SUBS] No sub streams left!'));
-        return null;
+    console.info(cyanBright(`[SUBS] ${langSubs.length} match language '${lang}'`));
+    if (langSubs.length == 0) {
+        console.warn(yellowBright('[SUBS] No sub streams left! Entering fallback mode.'));
+        subs = subs.filter(sub => sub.tags.language === 'und' || sub.tags.language == null);
+
+        console.info(cyanBright(`[SUBS] Found ${subs.length} fallback sub streams`));
+
+        if (subs.length == 0) {
+            console.warn(yellowBright(`[SUBS] No sub streams with language ${lang} or undefined.`));
+            return null;
+        } else if (subs.length == 1) {
+            console.info(cyanBright('[SUBS] Returning last remaining stream'));
+            return subs[0];
+        }
     } else if (subs.length == 1) {
         console.info(cyanBright('[SUBS] Returning last remaining stream'));
-        return subs[0];
+        return langSubs[0];
+    } else {
+        subs = langSubs;
     }
 
     console.info(cyanBright(`[SUBS] picking longest sub stream`));
     const subStreamsWithLengths = await Promise.all(subs.map(async subStream =>
         ({ len: await ffmpegGetStreamLength(data.format.filename, subStream.index, 'srt'), stream: subStream })
     ));
-    console.log(subStreamsWithLengths)
     const longest = subStreamsWithLengths.reduce((prev, cur) => cur.len > prev.len ? cur : prev, { len: 0 });
     console.info(cyanBright(`[SUBS] selected stream ${longest.stream.index}`));
 
